c84d5c46f934f8f4b23ca4f715dd5809572e4511
sayedmahmoud223 <sbendary977@gmail.com>
2025-02-20 03:56:59 +0200

    update

--- src/DB/dbConnection.ts
+++ src/DB/dbConnection.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 const connectDB = async () => {
-    console.log(process.env.DB_ONLINE_URL);
-    console.log(process.env.DB_OFFLINE_URL);
+    // console.log(process.env.DB_ONLINE_URL);
+    // console.log(process.env.DB_OFFLINE_URL);
     const { DB_ONLINE_URL, DB_OFFLINE_URL, MOOD } = process.env
     return await mongoose.connect(`${MOOD == "DEV" ? DB_OFFLINE_URL : DB_ONLINE_URL}`)
-        .then(res => console.log(`DB Connected successfully on .........`))
+        .then(res => console.log(`DB Connected successfully on ${MOOD == "DEV" ? DB_OFFLINE_URL : DB_ONLINE_URL}`))
         .catch(err => console.log(` Fail to connect  DB.........${err} `))
 }
 
