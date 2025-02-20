11c8e6d737b1389df9b0098731d04c594112ddc3
sayedmahmoud223 <sbendary977@gmail.com>
2025-02-20 04:12:54 +0200

    add frontend origin

--- src/DB/dbConnection.ts
+++ src/DB/dbConnection.ts
@@ -8,5 +8,4 @@ const connectDB = async () => {
         .catch(err => console.log(` Fail to connect  DB.........${err} `))
 }
 
-
 export default connectDB;
--- src/initApp/initApp.ts
+++ src/initApp/initApp.ts
@@ -10,7 +10,7 @@ export const BaseUrl = process.env.ONLINE_BASE_URL
 export const initApp = async (app: any, express: any) => {
     await connectDB()
     app.use(cors({
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:4200","https://dashboard-gules-seven.vercel.app"],
         credentials: true
     }))
     app.use(cookieParser())
